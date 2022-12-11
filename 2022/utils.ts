6833e3a45e2dba74f8c6d72c3ffd9977d223994c
david.schkalee <david.schkalee@sportalliance.com>
2022-12-11 17:43:39 +0100

    utils

---
+++ 2022/utils.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from 'fs';
+
+export function readFile(file: string) {
+    const pwd = process.argv[1];
+
+    return readFileSync(`${pwd.replace(/index\.ts$/, '')}/${file}`, 'utf-8');
+}
+
+export function lines(input: string) {
+    return input.split('\n');
+}
