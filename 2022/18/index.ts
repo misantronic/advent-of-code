448ca6dc10cefa05b8dbf66d9e9ea1f40a793fa9
david.schkalee <david.schkalee@sportalliance.com>
2022-12-19 12:24:22 +0100

    day 18 experiments

--- 2022/18/index.ts
+++ 2022/18/index.ts
@@ -39,4 +39,79 @@ function part1() {
     return cubes.length * 6 - covered;
 }
 
-console.log('part1 1:', part1());
+function inCubes(cube: Cube, needle = cubes) {
+    return needle.some(
+        ([x, y, z]) => x === cube[0] && y === cube[1] && z === cube[2]
+    );
+}
+
+function part2() {
+    const maxX = cubes.reduce((x, [cx]) => Math.max(x, cx), 0);
+    const maxY = cubes.reduce((y, [, cy]) => Math.max(y, cy), 0);
+    const maxZ = cubes.reduce((z, [, , cz]) => Math.max(z, cz), 0);
+
+    const candidates: Cube[] = [];
+
+    for (let cx = 0; cx <= maxX; cx++) {
+        for (let cy = 0; cy <= maxY; cy++) {
+            for (let cz = 0; cz <= maxZ; cz++) {
+                if (!inCubes([cx, cy, cz])) {
+                    candidates.push([cx, cy, cz]);
+                }
+            }
+        }
+    }
+
+    function getNeighbors(cube: Cube): Cube[] {
+        const [x, y, z] = cube;
+
+        return [
+            [x - 1, y, z],
+            [x + 1, y, z],
+            [x, y - 1, z],
+            [x, y + 1, z],
+            [x, y, z - 1],
+            [x, y, z + 1]
+        ].filter(
+            ([x, y, z]) =>
+                x >= 0 &&
+                x <= maxX &&
+                y >= 0 &&
+                y <= maxY &&
+                z >= 0 &&
+                z <= maxZ
+        ) as Cube[];
+    }
+
+    function scanSurroundings(cube: Cube) {
+        const neighbors = getNeighbors(cube);
+
+        if (neighbors.every((n) => inCubes(n))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    const trapped = candidates.reduce((trapped, cube) => {
+        if (!trapped.has(`${cube}`) && scanSurroundings(cube)) {
+            trapped.add(`${cube}`);
+        }
+
+        return trapped;
+    }, new Set<string>());
+
+    // 4078: too high
+    // 4054
+    // 3598
+    // 2728: too high
+    return part1() - trapped.size;
+}
+
+console.time('part 1');
+console.log('part 1:', part1());
+console.timeEnd('part 1');
+
+console.time('part 2');
+console.log('part 2:', part2());
+console.timeEnd('part 2');
