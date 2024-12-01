003202c39543af367b4e6837e0bb5f855b2a43a4
david.schkalee <david.schkalee@sportalliance.com>
2024-12-01 13:16:47 +0100

    2024 day 1 (py)

--- 2024/01/index.py
+++ 2024/01/index.py
@@ -8,18 +8,22 @@ def read_file(file_path):
 def lines(file_content):
     return [line.strip() for line in file_content]
 
-input1 = lines(read_file('input.txt'))
+input1 = lines(read_file('input-example.txt'))
 
 lists = [
-    [int(num) for num in re.findall(r'\d+', line)]
-    for line in input1
+    [int(num) for num in re.findall(r'\d+', line)] for line in input1
 ]
+
+# also works
+# lists = [list(map(int, re.findall(r'\d+', line))) for line in input1]
+
+print(lists)
 
 left = sorted([lst[0] for lst in lists])
 right = sorted([lst[1] for lst in lists])
 
 start_time = time.time()
-part1_result = sum(abs(right[i] - curr) for i, curr in enumerate(left))
+part1_result = sum(abs(right[i] - l) for i, l in enumerate(left))
 print('part 1:', part1_result)
 print('part 1 time:', time.time() - start_time)
 
