f7d139cef1c2a676078c9852887e6b0c515872a9
david.schkalee <david.schkalee@sportalliance.com>
2024-12-20 10:54:03 +0100

    2024 day 20 (pt. 1)

--- 2024/20/index.ts
+++ 2024/20/index.ts
@@ -1,10 +1,10 @@
 import { lines, PriorityQueue, readFile } from '../utils';
 
-const input1 = readFile('./input-example.txt');
-const input3 = readFile('./input.txt');
+const input1 = './input-example.txt';
+const input2 = './input.txt';
 
-[input1].forEach((c) => {
-    const grid = lines(c).map(
+[input1, input2].forEach((name) => {
+    const grid = lines(readFile(name)).map(
         (line) => line.split('') as ('.' | '#' | 'S' | 'E')[]
     );
 
@@ -36,26 +36,18 @@ const input3 = readFile('./input.txt');
         }
     }
 
-    const dijkstra = (grid: string[][]) => {
-        const queue = new PriorityQueue<
-            [number, number, number, number, Set<string>]
-        >([
-            {
-                item: [S[0], S[1], 0, 0, new Set([`${S[0]},${S[1]}`])],
-                priority: 0
-            }
+    const dijkstra = (grid: string[][], ignoreWall: number) => {
+        const queue = new PriorityQueue<[number, number, number]>([
+            { item: [S[0], S[1], 0], priority: 0 }
         ]);
-        const result: Set<string>[] = [];
 
         const visited = new Set<string>();
 
         while (!queue.isEmpty()) {
-            const [x, y, cost, cheat, path] = queue.dequeue()!;
+            const [x, y, cost] = queue.dequeue()!;
 
             if (x === E[0] && y === E[1]) {
-                console.log('found: cost', path.size, 'cheat:', cheat);
-                result.push(path);
-                continue;
+                return cost;
             }
 
             for (const [dx, dy] of dirs) {
@@ -63,65 +55,59 @@ const input3 = readFile('./input.txt');
                 const newCoord = `${nx},${ny}`;
 
                 if (
-                    nx < 0 ||
-                    nx >= grid[0].length ||
-                    ny < 0 ||
-                    ny >= grid.length ||
-                    (grid[ny][nx] === '#' && cheat >= 20) ||
-                    path.has(newCoord)
+                    walls[ignoreWall]?.[0] === nx &&
+                    walls[ignoreWall]?.[1] === ny
                 ) {
-                    continue;
+                    //
+                } else {
+                    if (
+                        nx < 0 ||
+                        nx >= grid[0].length ||
+                        ny < 0 ||
+                        ny >= grid.length ||
+                        grid[ny][nx] === '#' ||
+                        visited.has(newCoord)
+                    ) {
+                        continue;
+                    }
                 }
 
-                // console.log({ nx, ny, cheat });
+                queue.enqueue([nx, ny, cost + 1], cost + 1);
 
-                queue.enqueue(
-                    [
-                        nx,
-                        ny,
-                        cost + 1,
-                        grid[ny][nx] === '#' ? cheat + 1 : cheat,
-                        new Set([...path, newCoord])
-                    ],
-                    cost + 1
-                );
-
-                // path.add(newCoord);
+                visited.add(newCoord);
             }
         }
 
-        return result;
+        return -1;
     };
 
     console.time('part 1');
 
-    const base = dijkstra(grid);
+    const base = dijkstra(grid, -1);
 
-    console.log('base', base);
+    console.log('base cost', base);
 
     const costMap = new Map<number, number>();
 
-    // walls.forEach((_, i) => {
-    //     const save = base - dijkstra(grid);
+    walls.forEach((_, i) => {
+        const save = base - dijkstra(grid, i);
 
-    //     if (costMap.has(save)) {
-    //         costMap.set(save, costMap.get(save)! + 1);
-    //     } else {
-    //         costMap.set(save, 1);
-    //     }
-    // });
+        if (costMap.has(save)) {
+            costMap.set(save, costMap.get(save)! + 1);
+        } else {
+            costMap.set(save, 1);
+        }
+    });
 
-    // let total = 0;
+    let total = 0;
 
-    // for (const [costSave, count] of costMap) {
-    //     if (costSave >= 100) {
-    //         total += count;
-    //     }
-    // }
+    for (const [costSave, count] of costMap) {
+        if (costSave >= (name === './input.txt' ? 100 : 1)) {
+            total += count;
+        }
+    }
 
-    // console.log(costMap);
-
-    // console.log('part 1:', total);
+    console.log('part 1:', total);
     console.timeEnd('part 1');
 
     // console.time('part 2');
