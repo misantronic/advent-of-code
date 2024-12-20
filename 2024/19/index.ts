fc7fc632b5abba16682d1b529ec541b21639d01e
david.schkalee <david.schkalee@sportalliance.com>
2024-12-20 11:18:36 +0100

    2024 day 19 (pt. 1)

---
+++ 2024/19/index.ts
@@ -0,0 +1,44 @@
+import { lines, readFile } from '../utils';
+
+const input1 = './input-example.txt';
+const input2 = './input.txt';
+
+[input2].forEach((name) => {
+    const [patterns, , ...towels] = lines(readFile(name)).map((line, i) => {
+        if (i === 0) {
+            return line.split(', ');
+        }
+
+        return line;
+    }) as [string[], string, ...string[]];
+
+    const validTowels = towels.filter((towel, i) => {
+        const memo: { [key: number]: boolean } = {};
+
+        const dfs = (towel: string, index: number): boolean => {
+            if (index in memo) return memo[index];
+            if (index === towel.length) return true;
+
+            for (const pattern of patterns) {
+                if (towel.startsWith(pattern, index)) {
+                    if (dfs(towel, index + pattern.length)) {
+                        memo[index] = true;
+                        return true;
+                    }
+                }
+            }
+
+            memo[index] = false;
+
+            return false;
+        };
+
+        if (dfs(towel, 0)) {
+            return true;
+        }
+
+        return false;
+    });
+
+    console.log(name, 'part 1', validTowels.length);
+});
