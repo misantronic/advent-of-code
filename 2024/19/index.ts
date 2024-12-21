7eb95d7fb89859b602ee1cbbece25586066d8c9f
david.schkalee <david.schkalee@sportalliance.com>
2024-12-21 18:39:57 +0100

    2024 day 19 (optimize)

--- 2024/19/index.ts
+++ 2024/19/index.ts
@@ -3,7 +3,7 @@ import { lines, readFile } from '../utils';
 const input1 = './input-example.txt';
 const input2 = './input.txt';
 
-[input1].forEach((name) => {
+[input2].forEach((name) => {
     // patterns can be [r, wr, b, g, bwu, rb, gb, br]
     // towels can be [brwrr, bggr, gbbr, rrbgbr]
     const [patterns, , ...towels] = lines(readFile(name)).map((line, i) => {
@@ -15,29 +15,38 @@ const input2 = './input.txt';
     }) as [string[], string, ...string[]];
 
     const totalMap = new Map<string, number>();
+    const memo = new Map<string, number>();
 
     towels.forEach((towel, i) => {
-        const dfs = (towel: string, index: number, path: string[]) => {
-            if (towel.substring(index) === '') {
-                totalMap.set(towel, (totalMap.get(towel) || 0) + 1);
-
-                return true;
+        const dfs = (towel: string, index: number): number => {
+            if (index === towel.length) {
+                return 1;
             }
 
+            const key = `${towel}-${index}`;
+            if (memo.has(key)) {
+                return memo.get(key)!;
+            }
+
+            let count = 0;
             for (const pattern of patterns) {
                 if (towel.startsWith(pattern, index)) {
-                    dfs(towel, index + pattern.length, [...path, pattern]);
+                    count += dfs(towel, index + pattern.length);
                 }
             }
+
+            memo.set(key, count);
+            return count;
         };
 
-        dfs(towel, 0, []);
+        const count = dfs(towel, 0);
+        totalMap.set(towel, count);
     });
 
     console.log(name, 'part 1', totalMap.size);
     console.log(
         name,
         'part 1',
-        totalMap.values().reduce((a, b) => a + b, 0)
+        Array.from(totalMap.values()).reduce((a, b) => a + b, 0)
     );
 });
